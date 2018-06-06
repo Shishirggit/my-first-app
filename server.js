ea7e4ffbfb29f8a11d2362cc2361f04b47ba263c
unknown <leo.shishir105@gmail.com>
2018-06-06 21:05:52 +0530

    'Initing'

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const path = require('path');
 
 const app = express();
 
-console.log(path.join(__dirname,'dist/'));
+console.log(path.join(__dirname,'/dist'));
 
-app.use(express.static(path.join(__dirname,'dist/')));
+app.use(express.static(__dirname + '/dist'));
 
-const VIEWS = path.join(__dirname, 'dist');
+app.get('/*', function(req,res) {
+    
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-app.get("*", function(req,res){
+// const VIEWS = path.join(__dirname, 'dist');
+//	app.get("*", function(req,res){
   //res.sendFile(path.join(__dirname ,'/dist/index.html'));
-  res.sendFile('index.html', { root : VIEWS });
-})
+  //res.sendFile('index.html', { root : VIEWS });
+//})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server on port %d in %s mode", this.address().port, app.settings.env);
